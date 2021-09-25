0db680dc9dc7182e3c163ab003a7417bf28a513f
lennart <lsboecken@gmail.com>
2021-09-25 10:34:21 -0400

    finished functional side of todo app.

--- navbar.js
+++ navbar.js
@@ -1,9 +1,11 @@
 let navbar = document.querySelector('.navigation_bar');
 console.log(navbar);
 let html_template = `<ul id="navbar">
-<li><a href="/index.html">HOME</a></li>
-<li><a href="/contact.html">CONTACT</a></li>
-<li><a href="/clock.html">CLOCK</a></li>
+<li><a href="/index.html">Home</a></li>
+<li><a href="/contact.html">Contact Me</a></li>
+<li><a href="/clock.html">Clock</a></li>
+<li><a href="/todo.html">To Do's</a></li>
+
 </ul>`;
 
 navbar.innerHTML = html_template;
--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
-let ToDoCounter = 1;
-const inputField = document.getElementById('inputField');
-
+let ToDoCounter = 'ToDo1'; //used to create unique classes for each todo.
+const inputField = document.getElementById('inputField'); //used to create listener for input field.
+const ShowHideButton = document.getElementById('ShowHideButton'); //used to create listener for ShowHideButton
+let AreToDosHidden = false; //used to determine if finished todos are hidden.
 
 //main functions 
 
@@ -11,60 +12,83 @@ function CreateNewToDo(todo) {
     CreateFinishedButtonForNewToDo();
     CreateDeleteButtonForNewToDo();
     AttachNewToDoToUL();
-    
-    ToDoCounter++;
+    UpdateToDoCounter();
     inputField.value = null;
-
+    
     function CreateFinishedButtonForNewToDo() {
-        const NewButton = document.createElement('button');
-        NewButton.type = 'submit';
-        NewButton.id = ToDoCounter;
+        const NewButton = document.createElement('input');
+        NewButton.type = 'checkbox';
+        NewButton.className = ToDoCounter;
         NewButton.appendChild(document.createTextNode('Finished'))
+        NewButton.addEventListener('click', (e) => MarkToDoComplete(e))
         li_newToDo.appendChild(NewButton);
     }
     
     function CreateDeleteButtonForNewToDo() {
         const NewButton = document.createElement('button');
-        NewButton.type = 'submit';
-        NewButton.id = ToDoCounter;
-        NewButton.onclick = DeleteToDo;
+        NewButton.type = 'button';
+        NewButton.className = ToDoCounter;
         NewButton.appendChild(document.createTextNode('Delete'))
-        li_newToDo.appendChild(NewButton);
+        NewButton.addEventListener('click', (e) => DeleteToDo(e))
+        li_newToDo.appendChild(NewButton);    
     }
-
+    
     function CreateNewElementForNewToDo() {
         li_newToDo = document.createElement('li');
         li_newToDo.appendChild(document.createTextNode(todo));
-        li_newToDo.id = ToDoCounter
+        li_newToDo.className = ToDoCounter
     }
     
     function AttachNewToDoToUL() {
         ul = document.querySelector('#CurrentToDos');
         ul.insertBefore(li_newToDo, ul.childNodes[0]);
-
     }
+}    
+
+function MarkToDoComplete(e) {
+    className = 'li' + '.' + e.target.className;
+    Todo = document.querySelectorAll(className);
+    Todo = Todo[0];
+    Todo.style.textDecoration !== 'line-through' ? Todo.style.textDecoration = 'line-through' : Todo.style.textDecoration = 'none';
 }
 
-function MarkToDoComplete(id) {
-    let myElement = document.selectElementById(id)
-    ;
+function DeleteToDo(e) {
+    className = '.' + e.target.className;
+    Todo = document.querySelectorAll(className);
+    console.log(Todo);
+    Todo.forEach(element => element.remove());
 }
 
-function DeleteToDo() {
-    console.log('does this work?')    
+function ShowOrHideFinishedToDos() {
+    AllToDos = document.querySelectorAll('.todo li')
+    AllToDos.forEach(element => {
+        if (AreToDosHidden == true) {
+            if (element.style.textDecoration === 'line-through') {
+                element.style.display = 'block';
+            }
+        } else { 
+            if (element.style.textDecoration === 'line-through') {
+                element.style.display = 'none';
+                
+            }
+        }    
+    })
+    AreToDosHidden === false ? AreToDosHidden = true : AreToDosHidden = false;    
 }
 
-function HideFinishedToDo() {
-    null //BONUS -> hide all to dos that are done
-}
-
-function ShowFinishedToDo() {
-    null //BONUS -> show all to dos that are done
+function UpdateToDoCounter() {
+    ToDoCount = Number(ToDoCounter.slice(-1)) + 1;
+    ToDoCounter = 'ToDo' + ToDoCount;
 }
 
 //listeners
 
 inputField.addEventListener("keydown", (e) => 
-    {if (e.key == "Enter") CreateNewToDo(inputField.value);
+{if (e.key == "Enter") if (inputField.value !== '') {CreateNewToDo(inputField.value)}
 });
+
+ShowHideButton.addEventListener('click', ShowOrHideFinishedToDos)
+
+
+
 
