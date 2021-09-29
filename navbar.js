b64c4806fa28bdb5dc3faffe60d92058513ebfb8
lennart <lsboecken@gmail.com>
2021-09-28 20:03:52 -0400

    fixed CSS

--- navbar.js
+++ navbar.js
@@ -4,7 +4,7 @@ let html_template = `<ul id="navbar">
 <li><a href="/contact.html">Contact Me</a></li>
 <li><a href="/clock.html">Clock</a></li>
 <li><a href="/todo.html">To Do's</a></li>
-<li><a href="/expensetracker.html">To Do's</a></li>
+<li><a href="/expensetracker.html">ExpenseTracker</a></li>
 </ul>`;
 
 navbar.innerHTML = html_template;
